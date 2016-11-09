f676b667857cbebf0c21f6e359515f709155d462
1sky <1192099172@qq.com>
2016-11-09 19:45:51 +0800

    d

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
-$(function(){
-  $("#fullpage").fullpage({
+$(document).ready(function(){
+	$("#fullpage").fullpage({
     anchors:["shouye","a","b","c","d","e"],
-    scrollBar:true,
+    scrollBar:true
   })
 })
 var geren=angular.module("geren",[])
